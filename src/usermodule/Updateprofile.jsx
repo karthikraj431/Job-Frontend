ea97aa84cfbe0b318a6719d1a194dfc20800802a
Karthik Raj <56103545+karthikraj431@users.noreply.github.com>
2024-09-25 18:17:19 +0530

    Update Updateprofile.jsx

--- src/usermodule/Updateprofile.jsx
+++ src/usermodule/Updateprofile.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import Navuser from './Navuser'
+import { TextField, Typography } from '@mui/material'
 
 const Updateprofile = () => {
   return (
-    <div>
+    <div class="upd">
+        <div class="usernavv">
       <Navuser></Navuser>
+      <br/>
+      <Typography variant='h4'>UPDATE</Typography><br/><br/>
+        <TextField label='First Name' variant='filled'></TextField><br></br><br/>
+        <TextField label='Middle Name' variant='filled'></TextField><br/><br/>
+        <TextField label='Last Name' variant='filled'></TextField><br></br><br/>
+        <TextField label='DOB' variant='filled'></TextField><br></br><br/>
+        <TextField label='Age' variant='filled'></TextField><br/><br/>
+        <TextField label='Mobile Number' variant='filled'></TextField><br/><br/>
+        <TextField label='Email' variant='filled'></TextField><br/><br/>
+        <TextField label='Profession' variant='filled'></TextField><br/><br/>
+        <TextField label='Experience' variant='filled'></TextField><br/><br/>
+        </div>
     </div>
   )
 }
