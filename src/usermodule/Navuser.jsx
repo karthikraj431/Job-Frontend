6289a1f5c6cae29c24d525e4471b6915e3acb738
Karthik Raj <56103545+karthikraj431@users.noreply.github.com>
2024-09-25 18:19:44 +0530

    Update Navuser.jsx

--- src/usermodule/Navuser.jsx
+++ src/usermodule/Navuser.jsx
@@ -1,52 +1,90 @@
-import { AppBar, Box, Button, Divider, List, Toolbar, Typography } from '@mui/material'
-import React from 'react'
+import { AppBar, Avatar, Box, Button, Divider, Drawer, List, ListItemIcon, Toolbar, Typography } from '@mui/material'
+import React,{useState} from 'react'
 import { Link } from 'react-router-dom'
-import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
 
 const Navuser = () => {
+    const [open, setOpen] = React.useState(false);
+
+  const toggleDrawer = (newOpen) => () => {
+    setOpen(newOpen);
+  };
+  const [op, setOp] = React.useState(false);
+
+  const toggDraw = (newOp) => () => {
+    setOp(newOp);
+  };
+
+  const DrawerList = (
+    <Box sx={{ width: 250, color:"#00000000" }} role="presentation" onClick={toggleDrawer(false)}>
+      <List sx={{backgroundColor:"#00000000"}}>
+          <ListItem>
+          <Link to='/browser'>
+            <ListItemButton> 
+              <ListItemText primary="Browse Jobs" /><br/>
+              </ListItemButton>
+              </Link>
+              </ListItem>
+              <ListItem >
+              <Link to='/viewappjob'>
+              <ListItemButton> 
+              <ListItemText primary="View Applied Jobs" /><br/>
+              </ListItemButton>
+              </Link>
+              </ListItem>
+              <ListItem >
+              <Link to='/updateprof'>
+              <ListItemButton> 
+              <ListItemText primary="Update Profile" />
+            </ListItemButton>
+            </Link>
+          </ListItem><br/>
+          <ListItem >
+              <Link to='/'>
+              <ListItemButton> 
+              <ListItemText primary="Log out" />
+            </ListItemButton>
+            </Link>
+          </ListItem>
+
+      </List>
+    </Box>
+  );
+  const DrawerOut = (
+    <Box sx={{ width: 250, color:"#00000000" }} role="presentation" onClick={toggDraw(false)}>
+<Typography></Typography>
+
+    </Box>
+  );
   return (
     <div>
       <AppBar position='static' sx={{backgroundColor:'#00000000'}}>
               <Toolbar >
-              <IconButton
+              {/* <IconButton
             size="large"
             edge="start"
             color="inherit"
             aria-label="menu"
             sx={{ mr: 2 }}
-          >
-            <MenuIcon sx={{color:"black"}}></MenuIcon>
-          </IconButton>
-          <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
-      <Typography variant="h6" sx={{ my: 2 }}>
-        MUI
-      </Typography>
-      <Divider />
-      <List>
-        {navItems.map((item) => (
-          <ListItem key={item} disablePadding>
-            <ListItemButton sx={{ textAlign: 'center' }}>
-              <ListItemText primary={item} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </Box>
+          > */}
+            
+            <Button onClick={toggleDrawer(true)}><MenuIcon sx={{color:"black"}}/></Button>
+      <Drawer open={open} onClose={toggleDrawer(false)} sx={{color:"#00000000"}}>
+        {DrawerList}
+      </Drawer>
+          {/* </IconButton> */}
+          
+       
               <Typography align='left' variant='h6' component="div" sx={{ flexGrow: 1, color:"black"}}>USER DASHBOARD</Typography>
-              {/* <Link to='/userdash'>
-                      <Button variant='contained'>HOME</Button></Link>&nbsp;
-             <Link to='/browser'>
-                      <Button variant='contained'>BROWSER JOBS</Button></Link>&nbsp;
-             <Link to='/viewappjob'>
-                      <Button variant='contained'>VIEW APPLIED JOBS</Button></Link>&nbsp; 
-            <Link to='/updateprof'>
-                      <Button variant='contained'>UPDATE PROFILE</Button></Link>&nbsp;               */}
-                    
-              </Toolbar>
+      
+              <Button onClick={toggDraw(true)}><Avatar src="/broken-image.jpg" /></Button>
+      <Drawer open={open} onClose={toggDraw(false)} sx={{color:"#00000000", align:"right"}}>
+        {DrawerList}
+      </Drawer>
+                  </Toolbar>
           </AppBar>
     </div>
   )
